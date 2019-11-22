bda401afa50e47da28c6c88dce7d6c8758d143f3
Dimash1905 <57893153+Dimash1905@users.noreply.github.com>
2019-11-22 18:14:43 +0600

    day3

---
+++ task 133.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+	int k, n;
+	cin >> k;
+	int s = 0;
+	for (int i = 0; i < k; i++) {
+		s = s + i;
+		if (s == k) {
+			cout << i << endl;
+		}
+	}
+	return 0;
+}
